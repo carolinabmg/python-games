ef524c061dfb444916024deb3f9174052a23c16d
Carolina Bueno <petitearqstudy@gmail.com>
2026-06-12 17:06:23 -0300

    Add initial Hello World Python program with welcome messages

---
+++ Inicio python/Hello World.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+# This is a simple Python program that prints "Hello, World!" to the console.
+# To run this program, save it to a file named "Hello World.py" and execute
+# it using a Python interpreter. You should see the output "Hello, World!" displayed on the screen.
+
+print("Welcome to Python programming!")
+# This line adds an additional message to welcome users to Python programming.
+# You can modify this program to include more functionality or to print different messages.
+# Feel free to experiment with the code and explore more features of Python!
+
+print("Let's start coding!")
+# This line encourages users to start coding and explore the possibilities of Python.   
+# Remember, programming is a skill that improves with practice, so keep coding and have fun!
