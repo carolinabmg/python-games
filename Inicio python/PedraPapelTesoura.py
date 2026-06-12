7bc12b81b4628989579920d7796fc8a7a251926a
Carolina Bueno <petitearqstudy@gmail.com>
2026-06-12 19:14:41 -0300

    Refactor comments for clarity in Pedra, Papel, Tesoura game

--- Inicio python/PedraPapelTesoura.py
+++ Inicio python/PedraPapelTesoura.py
@@ -2,8 +2,8 @@ import random
 
 opcoes = ["pedra", "papel", "tesoura"]
 
-computador = random.choice(opcoes)
-jogador = input("Escolha pedra, papel ou tesoura: ").lower()
+computador = random.choice(opcoes) #random.choice() é uma função da biblioteca random que seleciona aleatoriamente um elemento de uma sequência (como uma lista). Neste caso, ela escolhe aleatoriamente entre "pedra", "papel" e "tesoura" para o computador.
+jogador = input("Escolha pedra, papel ou tesoura: ").lower() #.lower() para garantir que a entrada do usuário seja tratada como minúscula, independentemente de como ele digite.
 
 print(f"Você escolheu: {jogador}")
 print(f"Computador escolheu: {computador}")
