c367ad440a93b5466224d2cf46f890fe3c0fd79a
Carolina Bueno <petitearqstudy@gmail.com>
2026-06-12 21:12:54 -0300

    Add blank line for improved readability in Pedra, Papel, Tesoura game

--- Inicio python/PedraPapelTesoura.py
+++ Inicio python/PedraPapelTesoura.py
@@ -3,6 +3,7 @@ import random
 opcoes = ["pedra", "papel", "tesoura"]
 
 computador = random.choice(opcoes) #random.choice() é uma função da biblioteca random que seleciona aleatoriamente um elemento de uma sequência (como uma lista). Neste caso, ela escolhe aleatoriamente entre "pedra", "papel" e "tesoura" para o computador.
+
 jogador = input("Escolha pedra, papel ou tesoura: ").lower() #.lower() para garantir que a entrada do usuário seja tratada como minúscula, independentemente de como ele digite.
 
 print(f"Você escolheu: {jogador}")
